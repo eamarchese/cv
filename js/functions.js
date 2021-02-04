7ea6cefb3e23bb6e167c8a77f07f49bc0078fa2d
Enrico Agostini Marchese <enrico.agostini88@gmail.com>
2021-02-04 09:31:15 +0100

    central section media queries

--- js/functions.js
+++ js/functions.js
@@ -23,4 +23,35 @@ $( document ).ready(function() {
     $('.working.title').css('border-bottom', '2px #224669 solid');
     $('.skills.title').css('border-bottom', '2px #224669 solid');
   });
+  $('#education_mobile_button').click(function() {
+    $(".education_mobile.content").toggle(function(e) {
+        if ($(this).is(":visible")) {
+            $(".education_mobile.content").show();
+        }
+        else {
+            $(".education_mobile.content").hide();
+        };
+    });
+  });
+  $('#working_mobile_button').click(function() {
+    $(".working_mobile.content").toggle(function(e) {
+        if ($(this).is(":visible")) {
+            $(".working_mobile.content").show();
+        }
+        else {
+            $(".working_mobile.content").hide();
+        };
+    });
+  });
+  $('#skills_mobile_button').click(function() {
+    $(".skills_mobile.content").toggle(function(e) {
+        if ($(this).is(":visible")) {
+            $(".skills_mobile.content").show();
+        }
+        else {
+            $(".skills_mobile.content").hide();
+        };
+    });
+  });
+
 });
